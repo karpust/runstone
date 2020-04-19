e1e12a368fdc9509cd1b06917919b7c75f98e49f
karpust <513312@mail.ru>
2020-04-20 00:56:22 +0300

    0

--- 4. Basic Data Structures.py
+++ 4. Basic Data Structures.py
@@ -64,25 +64,140 @@
 # print(parChecker('[{()]'))
 
 
-"""4.8"""
+# """4.8"""
+# from pythonds.basic import Stack
+#
+# def baseConverter(decNumber,base):
+#     digits = "0123456789ABCDEF"
+#
+#     remstack = Stack()
+#
+#     while decNumber > 0:
+#         rem = decNumber % base
+#         remstack.push(rem)
+#         decNumber = decNumber // base
+#
+#     newString = ""
+#     while not remstack.isEmpty():
+#         newString = newString + digits[remstack.pop()]
+#
+#     return newString
+#
+# # print(baseConverter(25,2))
+# print(baseConverter(25, 8))
+
+
+# """4.9.2"""
+# from pythonds.basic import Stack
+#
+# def infixToPostfix(infixexpr):
+#     prec = {}
+#     prec["*"] = 3
+#     prec["/"] = 3
+#     prec["+"] = 2
+#     prec["-"] = 2
+#     prec["("] = 1
+#     opStack = Stack()
+#     postfixList = []
+#     tokenList = infixexpr.split()
+#
+#     for token in tokenList:
+#         if token in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" or token in "0123456789":
+#             postfixList.append(token)
+#         elif token == '(':
+#             opStack.push(token)
+#         elif token == ')':
+#             topToken = opStack.pop()
+#             while topToken != '(':
+#                 postfixList.append(topToken)
+#                 topToken = opStack.pop()
+#         else:
+#             while (not opStack.isEmpty()) and \
+#                (prec[opStack.peek()] >= prec[token]):
+#                   postfixList.append(opStack.pop())
+#             opStack.push(token)
+#
+#     while not opStack.isEmpty():
+#         postfixList.append(opStack.pop())
+#     return " ".join(postfixList)
+# #
+# print(infixToPostfix("A * ( B + C ) * D"))
+# print(infixToPostfix("( A + B ) * C - ( D - E ) * ( F + G )"))
+
+
+# """4.9.3"""
+# from pythonds.basic import Stack
+#
+# def postfixEval(postfixExpr):
+#     operandStack = Stack()
+#     tokenList = postfixExpr.split()
+#
+#     for token in tokenList:
+#         if token in "0123456789":
+#             operandStack.push(int(token))
+#         else:
+#             operand2 = operandStack.pop()
+#             operand1 = operandStack.pop()
+#             result = doMath(token,operand1,operand2)
+#             operandStack.push(result)
+#     return operandStack.pop()
+#
+# def doMath(op, op1, op2):
+#     if op == "*":
+#         return op1 * op2
+#     elif op == "/":
+#         return op1 / op2
+#     elif op == "+":
+#         return op1 + op2
+#     else:
+#         return op1 - op2
+#
+# print(postfixEval('7 8 + 3 2 + /'))
+
+"""4.9.3
+Q-3: Modify the infixToPostfix function so that
+it can convert the following expression:
+5 * 3 ** (4 - 2). Run the function on the expression
+and paste the answer here:"""
+
 from pythonds.basic import Stack
 
-def baseConverter(decNumber,base):
-    digits = "0123456789ABCDEF"
+expression = '5 * 3 ** (4 - 2)'
 
-    remstack = Stack()
+def infixToPostfix(expr):
+    expr = expr.split()
+    digits = '1234567890'
+    new_expr = []
+    OperandStack = Stack()
+    prec = {}
+    prec['*'] = 3
+    prec['/'] = 3
+    prec['+'] = 2
+    prec['-'] = 2
+    prec['**'] = 4
+    prec['('] = 1
+    for el in expr:
+        if el in digits:
+            new_expr.append(el)
+        elif el == '(':
+            OperandStack.push(el)
+        elif el == ')':
+            topToken = OperandStack.pop()
+            while topToken != '(':
+                topToken = OperandStack.pop()
+                new_expr.append(topToken)
+        else:
+            while not OperandStack.isEmpty()and el <= OperandStack.peek():
+                new_expr.append(OperandStack.pop())
+            OperandStack.push(el)
+    return new_expr
 
-    while decNumber > 0:
-        rem = decNumber % base
-        remstack.push(rem)
-        decNumber = decNumber // base
 
-    newString = ""
-    while not remstack.isEmpty():
-        newString = newString + digits[remstack.pop()]
+# def compute(expr):
+#     if
 
-    return newString
 
-# print(baseConverter(25,2))
-print(baseConverter(256,16))
+print(infixToPostfix(expression))
+
+
 
